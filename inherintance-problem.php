71f99dc1ee4c19f740a5411b87a61a4ddf267c23
Griswalde <agunggriswald158@gmail.com>
2021-09-23 09:31:59 +0700

    menyelesaikan video ke 1 tugas 2 , membuat inheritance

--- inherintance-problem.php
+++ inherintance-problem.php
@@ -1,22 +1,23 @@
 <?php
-class Produk {
+class Produk
+{
   public $judul,
-          $penulis,
-          $penerbit,
-          $harga,
-          $jmlHalaman,
-          $waktuMain,
-          $tipe;
-  public function __construct($judul = "judul", $penulis = "penulis", $penerbit ="penerbit", $harga = 0, 
-  $jmlHalaman = 0, $waktuMain = 0){
+    $penulis,
+    $penerbit,
+    $harga,
+    $jmlHalaman,
+    $waktuMain,
+    $tipe;
+
+  public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHalaman = 0, $waktuMain = 0, $tipe = "tipe")
   {
-    $this->judul    = $judul;
-    $this->penulis  = $penulis;
-    $this->penerbit = $penerbit;
-    $this->harga    = $harga;
+    $this->judul      = $judul;
+    $this->penulis    = $penulis;
+    $this->penerbit   = $penerbit;
+    $this->harga      = $harga;
     $this->jmlHalaman = $jmlHalaman;
-    $this->waktuMain = $waktuMain;
-    $this->tipe = $tipe;
+    $this->waktuMain  = $waktuMain;
+    $this->tipe       = $tipe;
   }
 
   public function getLabel()
@@ -24,38 +25,32 @@ class Produk {
     return "$this->penulis, $this->penerbit";
   }
 
-  public function getInfoLengkap() {
-    // komik : naruto | Mashashi Kishimoto, Shonen Jump(rp .3000) - 100 halaman.
-    $str = "{$this->tipe} : {$this->judul} | {$this->getLabel()} (Rp, {$this->harga})";
-    if($this->tipe == "Komik") {
+  public function getInfoLengkap()
+  {
+    $str = "{$this->tipe} : {$this->judul} | {$this->getLabel()} ({$this->harga})";
+    if ($this->tipe === "Komik") {
       $str .= " - {$this->jmlHalaman} Halaman.";
-    } else if ($this->tipe =="Game") {
-      $str .= " ~{$this->waktuMain} Jam.";
+    } else if ($this->tipe === "Game") {
+      $str .= " ~ {$this->waktuMain} Jam.";
     }
+
     return $str;
   }
 }
 
-class CetakInfoProduk {
-
-  public function cetak($produk)
+class CetakInfoProduk
+{
+  public function cetak(Produk $produk)
   {
-    $str="{$produk->judul} | {$produk->getLabel()} (Rp.{$produk->harga})";
-    return $str; 
+    $str = "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
+    return $str;
   }
 }
 
-$produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100,0,"komik");
-$produk2 = new Produk("Uncharted", "Neil Druckermann", "Sony Computer", 25000,0, 50);
+$produk1 = new Produk("Naruto", "Masashi Kishimoto", "Sonen jump", 30000, 100, 0, "Komik");
+$produk2 = new Produk("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 0, 50, "Game");
 
 echo $produk1->getInfoLengkap();
-echo "<br>";
+echo "</br>";
 echo $produk2->getInfoLengkap();
-
-//Komik : Masashi Kishimoto, Shonen Jump
-//Game : Neil Druckermann, Sony Computer
-//Naruto | Masashi Kishimoto, Shonen Jump (Rp.30000)
-
-// komik : naruto | Mashashi Kishimoto, Shonen Jump(rp .3000) - 100 halaman
-// Game : Uncharted | Neil Druckman, Sony Computer (rp.250000) - 50jam
-a
+echo "</br>";
